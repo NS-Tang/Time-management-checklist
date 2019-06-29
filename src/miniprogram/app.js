62fed27db5e501a47c4d83a4a7b6f76186156965
155773 <1557735451@qq.com>
2019-06-29 22:26:12 +0800

    newsummary
    合并

--- src/miniprogram/app.js
+++ src/miniprogram/app.js
@@ -1,4 +1,5 @@
 //app.js
+import { log, promiseHandle } from 'utils/util';
 App({
   onLaunch: function () {
     
@@ -11,5 +12,30 @@ App({
     }
 
     this.globalData = {}
+  },
+
+   getUserInfo(cb) {
+    if (typeof cb !== "function") return;
+    let that = this;
+    if (that.globalData.userInfo) {
+      cb(that.globalData.userInfo);
+    } else {
+      promiseHandle(wx.login).then(() => promiseHandle(wx.getUserInfo)).then(res => {
+        that.globalData.userInfo = res.userInfo;
+        cb(that.globalData.userInfo);
+      }).catch(err => {
+        log(err);
+      });
+    }
+  },
+
+  globalData: {
+    userInfo: null
+  },
+
+  //自定义配置
+  settings: {
+    debug: true, //是否调试模式
   }
 })
+
