3ea61fe1d3782f5a952135ad6fb65441e379c2fb
NS-Tang <13521060078@163.com>
2019-06-30 14:30:36 +0800

    更改日历中添加事项的界面

--- src/miniprogram/pages/addFunction/addFunction.js
+++ src/miniprogram/pages/addFunction/addFunction.js
@@ -1,60 +1,66 @@
 // pages/addFunction/addFunction.js
-
-const code = `// 云函数入口函数
-exports.main = (event, context) => {
-  console.log(event)
-  console.log(context)
-  return {
-    sum: event.a + event.b
-  }
-}`
-
 Page({
 
+  /**
+   * 页面的初始数据
+   */
   data: {
-    result: '',
-    canIUseClipboard: wx.canIUse('setClipboardData'),
+
   },
 
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
 
   },
 
-  copyCode: function() {
-    wx.setClipboardData({
-      data: code,
-      success: function () {
-        wx.showToast({
-          title: '复制成功',
-        })
-      }
-    })
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
   },
 
-  testFunction() {
-    wx.cloud.callFunction({
-      name: 'sum',
-      data: {
-        a: 1,
-        b: 2
-      },
-      success: res => {
-        wx.showToast({
-          title: '调用成功',
-        })
-        this.setData({
-          result: JSON.stringify(res.result)
-        })
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '调用失败',
-        })
-        console.error('[云函数] [sum] 调用失败：', err)
-      }
-    })
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
   },
 
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 })
-
