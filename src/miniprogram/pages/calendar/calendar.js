fa7e61644bc637f3cc97ab5f437463a4e2048421
NS-Tang <13521060078@163.com>
2019-07-01 17:39:16 +0800

    debug

--- src/miniprogram/pages/calendar/calendar.js
+++ src/miniprogram/pages/calendar/calendar.js
@@ -153,7 +153,6 @@ Page({
         //清空表单
         this.setData({
           todoTextAreaValue: '',
-          // levelSelectedValue: LEVEL.normal,
           todoInputValue: ''
         });
         loadItemListData.call(this);
